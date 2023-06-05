a06f991bdec357f0dc81f1030bea39c0d04eda0f
Emile <bvcxzq1@gmail.com>
2023-06-05 20:23:17 +0000

    Play with basic Class and Tests

---
+++ student.py
@@ -0,0 +1,43 @@
+from datetime import date, timedelta
+import requests
+
+"""
+Can implement
+Return Students Start date
+Factor in leap years
+Display any extention given
+Add student scores
+"""
+
+
+class Student:
+    """ A Student class as a basis for method testing """
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
+
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
